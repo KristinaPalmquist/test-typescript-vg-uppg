51fe9ff32bb96bf3b261ed018512950b3efa54d5
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-12-20 12:02:12 +0100

    added jest.config

--- validateEmail.spec.ts
+++ validateEmail.spec.ts
@@ -1,47 +1,44 @@
-import { validateEmail } from "./validateEmail"
+import { validateEmail } from "./validateEmail";
 
-
-
-
-describe('validateEmail', () => {
-  it('should return true if the email is valid', () => {
-    const actualResponse = validateEmail('anna.andersson@gmail.com')
-    const expectedResponse = true
+describe("validateEmail", () => {
+  it("should return true if the email is valid", () => {
+    const actualResponse = validateEmail("anna.andersson@gmail.com");
+    const expectedResponse = true;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('@gmail.com')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("@gmail.com");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('anna.anderssongmail.com')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("anna.anderssongmail.com");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('anna.andersson@.com')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("anna.andersson@.com");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('anna.andersson@gmail.')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("anna.andersson@gmail.");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('anna.andersson@gmail')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("anna.andersson@gmail");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('gmail.com')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("gmail.com");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-  it('should return false if the email is invalid', () => {
-    const actualResponse = validateEmail('anna.andersson.gmail.com')
-    const expectedResponse = false
+  });
+  it("should return false if the email is invalid", () => {
+    const actualResponse = validateEmail("anna.andersson.gmail.com");
+    const expectedResponse = false;
     expect(actualResponse).toEqual(expectedResponse);
-  })
-})
+  });
+});
